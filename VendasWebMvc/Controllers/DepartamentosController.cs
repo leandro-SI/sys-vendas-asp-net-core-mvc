999dd118e806af894b4e3a1654e4f7b4b6f966d1
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2019-01-15 23:54:52 -0200

    First Model-Controller-View - Department

---
+++ VendasWebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departamento> list = new List<Departamento>();
+
+            list.Add(new Departamento{ Id = 1, Name = "Eletrônicos"});
+            list.Add(new Departamento{ Id = 2, Name = "Fashion"});
+            list.Add(new Departamento { Id = 3, Name = "informatica" });
+
+
+            return View(list);
+        }
+    }
+}
