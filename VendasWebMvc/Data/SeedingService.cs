11a0393ec090521740b01ae416f407be0e83ebf4
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2019-01-24 21:40:47 -0200

    Seeding Service

---
+++ VendasWebMvc/Data/SeedingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Models;
+using VendasWebMvc.Models.Enums;
+
+namespace VendasWebMvc.Data
+{
+    public class SeedingService
+    {
+        private VendasWebMvcContext _context;
+
+        public SeedingService(VendasWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if(_context.Departamento.Any() || 
+                _context.Vendedor.Any() ||
+                _context.registroDeVendas.Any())
+            {
+                return; // O banco de dados já foi populado
+            }
+
+            Departamento d1 = new Departamento(1, "Computadores");
+            Departamento d2 = new Departamento(2, "Eletronicos");
+            Departamento d3 = new Departamento(3, "Moda");
+            Departamento d4 = new Departamento(4, "Livros");
+
+            Vendedor v1 = new Vendedor(1, "Boby brow", "boby@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+            Vendedor v2 = new Vendedor(2, "Maria elisa", "maria@gmail.com", new DateTime(1998, 4, 21), 1000.0, d2);
+            Vendedor v3 = new Vendedor(3, "Joana Dark", "joana@gmail.com", new DateTime(1998, 4, 21), 1000.0, d3);
+            Vendedor v4 = new Vendedor(4, "Messias Bolsonaro", "bolsonaro@gmail.com", new DateTime(1998, 4, 21), 1000.0, d4);
+
+            RegistroDeVendas r1 = new RegistroDeVendas(1, new DateTime(2018, 9, 25), 11000.00, SaleStatus.Faturado, v1);
+            RegistroDeVendas r2 = new RegistroDeVendas(2, new DateTime(2018, 9, 25), 11000.00, SaleStatus.Faturado, v2);
+            RegistroDeVendas r3 = new RegistroDeVendas(3, new DateTime(2018, 9, 25), 11000.00, SaleStatus.Faturado, v1);
+            RegistroDeVendas r4 = new RegistroDeVendas(4, new DateTime(2018, 9, 25), 11000.00, SaleStatus.Faturado, v2);
+
+            _context.Departamento.AddRange(d1, d2, d3, d4);
+            _context.Vendedor.AddRange(v1, v2, v3, v4);
+            _context.registroDeVendas.AddRange(r1, r2, r3, r4);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- VendasWebMvc/Startup.cs
+++ VendasWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using VendasWebMvc.Models;
+using VendasWebMvc.Data;
 
 namespace VendasWebMvc
 {
@@ -39,14 +40,17 @@ namespace VendasWebMvc
             services.AddDbContext<VendasWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("VendasWebMvcContext"), builder =>
                     builder.MigrationsAssembly("VendasWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
